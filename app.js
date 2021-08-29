65103cd656a8e45c3805f9a9fd25ab6560aa1aad
shubh-am003 <69967560+shubh-am003@users.noreply.github.com>
2021-08-29 22:30:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+// my routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+const stripeRoutes = require("./routes/stripepayments");
+
+// connect to database
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED!");
+  });
+
+// middlewares
+app.use(function (req, res, next) {
+  // console.log("request: " + JSON.stringify(req));
+  res.header("Access-Control-Allow-Origin", "*");
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept"
+  // );
+  next();
+});
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+// routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", stripeRoutes);
+
+// ports
+const port = process.env.PORT || 8000;
+
+// start server
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
